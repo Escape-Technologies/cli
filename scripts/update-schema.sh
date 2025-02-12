dbe5d18a8aede4a433600cc3e0635edcd5369833
QuentinN42 <quentin@lieumont.fr>
2025-02-12 21:45:51 +0100

    fix: bump schema and add locations crud
    Signed-off-by: QuentinN42 <quentin@lieumont.fr>

--- scripts/update-schema.sh
+++ scripts/update-schema.sh
@@ -4,8 +4,12 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PROJECT_ROOT="$(dirname "$SCRIPT_DIR")"
 
-# Retrieve the schema from the public API
-curl -s https://public.escape.tech/v1/openapi.json > $PROJECT_ROOT/assets/public-api.json
+(
+    cd "${PROJECT_ROOT}"
 
-# Generate the code binding on openAPI schema
-go generate $PROJECT_ROOT/pkg/api/...
+    # Retrieve the schema from the public API
+    curl -s https://public.escape.tech/v1/openapi.json | jq > "${PROJECT_ROOT}/assets/public-api.json"
+
+    # Generate the code binding on openAPI schema
+    go generate ./...
+)
