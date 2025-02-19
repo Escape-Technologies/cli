436c460c92bb3ddb062843154cc257e60488d334
Kaan Doyurur <kaan@escape.tech>
2025-02-19 18:20:42 +0100

    --wip-- [skip ci]

--- pkg/locations/start.go
+++ pkg/locations/start.go
@@ -11,23 +11,22 @@ import (
 
 
 func Start(ctx context.Context, client *api.ClientWithResponses, name string) error {
-	publicSSHKey, privateSSHKey := privatelocation.GenSSHKeys()
-	log.Info("Private SSH Key: %s", privateSSHKey)
-	log.Info("Public SSH Key: %s", publicSSHKey)
+	sshPublicKey, sshPrivateKey := privatelocation.GenSSHKeys(name)
+	log.Info("Generated public SSH Key: %s", sshPublicKey)
 
-	log.Info("Upserting location %s", name)
+	log.Info("Upserting location %s with public key %s", name, sshPublicKey)
 	y := true
 	location, err := client.UpsertLocationWithResponse(ctx, api.UpsertLocationJSONRequestBody{
 		Name:              name,
 		PrivateLocationV2: &y,
-		Key:               &publicSSHKey,
+		Key:               &sshPublicKey,
 	})
 	if err != nil {
 		return err
 	}
 
 	if location.JSON200 != nil {
-		return privatelocation.StartLocation(ctx, string(privateSSHKey))
+		return privatelocation.StartLocation(ctx,location.JSON200.Id.String(), sshPrivateKey)
 	} else if location.JSON400 != nil {
 		return fmt.Errorf("unable to start location: %s", location.JSON400.Message)
 	} else if location.JSON500 != nil {
