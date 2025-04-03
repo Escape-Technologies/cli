cbc30e5369375cc7ec53fd763629e74b46d312cb
QuentinN42 <quentin@lieumont.fr>
2025-04-03 14:49:53 +0200

    feat: domain and subdomains
    Signed-off-by: QuentinN42 <quentin@lieumont.fr>

---
+++ pkg/api/escape/domains.go
@@ -0,0 +1,45 @@
+package escape
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
+
+	openapi_types "github.com/oapi-codegen/runtime/types"
+
+	v2 "github.com/Escape-Technologies/cli/pkg/api/v2"
+)
+
+type Domain struct {
+	CreatedAt    time.Time          `json:"createdAt"`
+	Id           openapi_types.UUID `json:"id"`
+	ServiceCount float32            `json:"serviceCount"`
+}
+
+func GetDomains(ctx context.Context) ([]Domain, error) {
+	client, err := v2.NewAPIClient()
+	if err != nil {
+		return nil, fmt.Errorf("unable to init client: %w", err)
+	}
+	resp, err := client.GetDomainsWithResponse(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("unable to get domains: %w", err)
+	}
+	if resp.StatusCode() != http.StatusOK {
+		return nil, fmt.Errorf("unable to get domains: status code %d", resp.StatusCode())
+	}
+	if resp.JSON200 == nil {
+		return nil, errors.New("unable to get domains: no data received")
+	}
+	domains := []Domain{}
+	for _, domain := range *resp.JSON200 {
+		domains = append(domains, Domain{
+			CreatedAt:    domain.CreatedAt,
+			Id:           domain.Id,
+			ServiceCount: domain.ServiceCount,
+		})
+	}
+	return domains, nil
+}
---
+++ pkg/api/escape/subdomains.go
@@ -0,0 +1,45 @@
+package escape
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+
+	v2 "github.com/Escape-Technologies/cli/pkg/api/v2"
+)
+
+type Subdomain struct {
+	Id            string  `json:"id"`
+	Fqdn          string  `json:"fqdn"`
+	ServicesCount float32 `json:"servicesCount"`
+}
+
+func GetSubdomains(ctx context.Context, count *int, after *string) ([]Subdomain, *string, error) {
+	client, err := v2.NewAPIClient()
+	if err != nil {
+		return nil, nil, fmt.Errorf("unable to init client: %w", err)
+	}
+	resp, err := client.GetSubdomainsWithResponse(ctx, &v2.GetSubdomainsParams{
+		Count: count,
+		After: after,
+	})
+	if err != nil {
+		return nil, nil, fmt.Errorf("unable to get subdomains: %w", err)
+	}
+	if resp.StatusCode() != http.StatusOK {
+		return nil, nil, fmt.Errorf("unable to get subdomains: status code %d", resp.StatusCode())
+	}
+	if resp.JSON200 == nil {
+		return nil, nil, errors.New("unable to get subdomains: no data received")
+	}
+	subdomains := []Subdomain{}
+	for _, subdomain := range (*resp.JSON200).Data {
+		subdomains = append(subdomains, Subdomain{
+			Id:            subdomain.Id,
+			Fqdn:          subdomain.Fqdn,
+			ServicesCount: subdomain.ServicesCount,
+		})
+	}
+	return subdomains, resp.JSON200.NextCursor, nil
+}
