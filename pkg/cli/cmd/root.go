6d10060ac67ec01911d38442b06c5209c018f9c9
QuentinN42 <quentin@lieumont.fr>
2025-04-16 09:30:32 +0200

    fix: help all
    Signed-off-by: QuentinN42 <quentin@lieumont.fr>

--- pkg/cli/cmd/root.go
+++ pkg/cli/cmd/root.go
@@ -2,6 +2,9 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"strings"
+	"text/tabwriter"
 
 	"github.com/Escape-Technologies/cli/pkg/cli/out"
 	"github.com/Escape-Technologies/cli/pkg/log"
@@ -30,9 +33,43 @@ var rootCmd = &cobra.Command{
 	},
 }
 
+// Recursive function to print command names and help
+func listCommands(cmd *cobra.Command, prefix string) []string {
+	commands := []string{}
+	for _, c := range cmd.Commands() {
+		if prefix == "" && (c.Name() == "help-all" ||
+			c.Name() == "help" ||
+			c.Name() == "completion") {
+			continue
+		}
+		line := fmt.Sprintf("%s%s\t%s", prefix, c.Name(), c.Short)
+		commands = append(commands, line)
+		commands = append(commands, listCommands(c, prefix+"  ")...)
+	}
+	return commands
+}
+
+var helpAllCmd = &cobra.Command{
+	Use:   "help-all",
+	Short: "List all commands",
+	Run: func(cmd *cobra.Command, args []string) {
+		commands := listCommands(rootCmd, "")
+		w := tabwriter.NewWriter(os.Stdout, 0, 0, 4, ' ', 0)
+		fmt.Fprintln(w, "COMMAND\tDESCRIPTION")
+		for _, command := range commands {
+			if !strings.HasPrefix(command, " ") {
+				fmt.Fprintln(w, "\t")
+			}
+			fmt.Fprintln(w, command)
+		}
+		w.Flush()
+	},
+}
+
 func init() {
 	rootCmd.PersistentFlags().BoolVarP(&rootCmdVerbose, "verbose", "v", false, "enable verbose output")
 	rootCmd.PersistentFlags().StringVarP(&rootCmdOutputStr, "output", "o", "pretty", "output format (pretty|json|yaml)")
+	rootCmd.AddCommand(helpAllCmd)
 }
 
 func Execute() error {
