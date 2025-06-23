bbb01f54a334a45a8aaf36ab9e4061507e289873
Kaan Doyurur <kaan@escape.tech>
2025-06-23 20:18:24 +0200

    fix: magic
    Signed-off-by: Kaan Doyurur <kaan@escape.tech>

--- pkg/locations/private/monitor/logs.go
+++ pkg/locations/private/monitor/logs.go
@@ -7,6 +7,8 @@ import (
 	"golang.org/x/crypto/ssh"
 )
 
+const maxLogLevel = 4
+
 func sendLogs(ctx context.Context, ch ssh.Channel) {
 	go func() {
 		<-ctx.Done()
@@ -14,7 +16,7 @@ func sendLogs(ctx context.Context, ch ssh.Channel) {
 	}()
 	log.AddHook("monitor", func(log log.Entry) {
 		// Log levels: trace: 6, debug: 5, info: 4, warn: 3, error: 2, fatal: 1, panic: 0
-		if log.Level <= 4 {
+		if log.Level <= maxLogLevel {
 			ch.SendRequest("log", false, []byte(log.Message)) //nolint:errcheck
 		}
 	})
