544fcb688ea1953915e5bc9c3cd27535eec24e0c
Kaan Doyurur <kaan@escape.tech>
2025-06-23 19:06:16 +0200

    chore: do not send debug logs
    Signed-off-by: Kaan Doyurur <kaan@escape.tech>

--- pkg/locations/private/monitor/logs.go
+++ pkg/locations/private/monitor/logs.go
@@ -13,6 +13,9 @@ func sendLogs(ctx context.Context, ch ssh.Channel) {
 		log.RemoveHook("monitor")
 	}()
 	log.AddHook("monitor", func(log log.Entry) {
-		ch.SendRequest("log", false, []byte(log.Message)) //nolint:errcheck
+		// Log levels: trace: 6, debug: 5, info: 4, warn: 3, error: 2, fatal: 1, panic: 0
+		if log.Level <= 4 {
+			ch.SendRequest("log", false, []byte(log.Message)) //nolint:errcheck
+		}
 	})
 }
