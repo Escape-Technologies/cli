c3464ace547581cc1e139b0901674b7346783a0a
Valentin REJAUNIER <valentin@escape.tech>
2025-08-13 16:19:05 +0200

    feat: add more methods to profile cli

--- pkg/api/escape/profiles.go
+++ pkg/api/escape/profiles.go
@@ -2,6 +2,7 @@ package escape
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	v3 "github.com/Escape-Technologies/cli/pkg/api/v3"
@@ -37,4 +38,108 @@ func GetProfile(ctx context.Context, profileID string) (*v3.ProfileDetailed, err
 		return nil, fmt.Errorf("api error: %w", err)
 	}
 	return data, nil
+}
+
+// DeleteProfile deletes a profile by ID
+func DeleteProfile(ctx context.Context, profileID string) error {
+	client, err := newAPIV3Client()
+	if err != nil {
+		return fmt.Errorf("unable to init client: %w", err)
+	}
+	_, _, err = client.ProfilesAPI.DeleteProfile(ctx, profileID).Execute()
+	if err != nil {
+		return fmt.Errorf("api error: %w", err)
+	}
+	return nil
+}
+
+// UpdateProfile updates a profile by ID
+func UpdateProfile(
+	ctx context.Context,
+	profileID string,
+	profileName *string,
+	profileCron *string,
+) error {
+	client, err := newAPIV3Client()
+	req := client.ProfilesAPI.UpdateProfile(ctx, profileID)
+	if err != nil {
+		return fmt.Errorf("unable to init client: %w", err)
+	}
+
+	updateProfileRequest := v3.UpdateProfileRequest{}
+	if profileName != nil {
+		updateProfileRequest.Name = profileName
+	}
+	if profileCron != nil {
+		updateProfileRequest.Cron = profileCron
+	}
+	req = req.UpdateProfileRequest(updateProfileRequest)
+
+	_, _, err = req.Execute()
+	if err != nil {
+		return fmt.Errorf("api error: %w", err)
+	}
+	return nil
+}
+
+// UpdateProfileSchema updates a profile schema by profileID
+func UpdateProfileSchema(
+	ctx context.Context,
+	profileID string,
+	newProfileSchemaURL *string,
+) error {
+	client, err := newAPIV3Client()
+	if err != nil {
+		return fmt.Errorf("unable to init client: %w", err)
+	}
+
+	if newProfileSchemaURL != nil {
+		updateProfileSchemaRequest := v3.UpdateProfileSchemaRequest{
+			ProfileId: profileID,
+			SchemaUrl: *newProfileSchemaURL,
+		}
+		req := client.ProfilesAPI.UpdateProfileSchema(ctx, profileID).UpdateProfileSchemaRequest(updateProfileSchemaRequest)
+
+		_, _, err := req.Execute()
+		if err != nil {
+			return fmt.Errorf("api error: %w", err)
+		}
+
+		return nil
+	}
+
+	return errors.New("no new profile schema url provided")
+}
+
+// UpdateProfileConfiguration updates a profile schema by profileID
+func UpdateProfileConfiguration(
+	ctx context.Context,
+	profileID string,
+	configuration *map[string]string,
+) error {
+	client, err := newAPIV3Client()
+	if err != nil {
+		return fmt.Errorf("unable to init client: %w", err)
+	}
+
+	if configuration != nil {
+		cfg := make(map[string]interface{}, len(*configuration))
+		for k, v := range *configuration {
+			cfg[k] = v
+		}
+		updateProfileConfigurationRequest := v3.UpdateProfileConfigurationRequest{
+			Configuration: cfg,
+		}
+		req := client.ProfilesAPI.UpdateProfileConfiguration(ctx, profileID).UpdateProfileConfigurationRequest(updateProfileConfigurationRequest)
+
+		_, apiResp, err := req.Execute()
+		fmt.Println(apiResp)
+		if err != nil {
+			return fmt.Errorf("api error: %w", err)
+		}
+
+		return nil
+	}
+
+	return errors.New("no configuration provided")
 }
--- pkg/cli/cmd/profiles.go
+++ pkg/cli/cmd/profiles.go
@@ -8,6 +8,13 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	newProfileName          string
+	newProfileCron          string
+	newProfileSchemaURL     string
+	newProfileConfiguration map[string]string
+)
+
 var profilesCmd = &cobra.Command{
 	Use:     "profiles",
 	Aliases: []string{"profile", "profiles"},
@@ -85,10 +92,105 @@ ID                                      TYPE         NAME                       
 	},
 }
 
+var profileDeleteCmd = &cobra.Command{
+	Use:     "delete profile-id",
+	Aliases: []string{"delete"},
+	Short:   "Delete a profile",
+	Long: `Get a profile by ID.
+
+Example output:
+Profile 00000000-0000-0000-0000-000000000001 successfully deleted`,
+	Example: `escape-cli profiles delete 00000000-0000-0000-0000-000000000001`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		profileID := args[0]
+		err := escape.DeleteProfile(cmd.Context(), profileID)
+		if err != nil {
+			return fmt.Errorf("unable to delete profile %s: %w", profileID, err)
+		}
+
+		fmt.Printf("Profile %s successfully deleted\n", profileID)
+
+		return nil
+	},
+}
+
+var profileUpdateCmd = &cobra.Command{
+	Use:     "update profile-id",
+	Aliases: []string{"update"},
+	Short:   "Update a profile",
+	Long: `Update a profile by ID.
+Example output:
+Profile 00000000-0000-0000-0000-000000000001 successfully updated`,
+	Example: `escape-cli profiles update 00000000-0000-0000-0000-000000000001 --name "New Name" --cron "0 11 * * 5"`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		profileID := args[0]
+
+		err := escape.UpdateProfile(
+			cmd.Context(),
+			profileID,
+			&newProfileName,
+			&newProfileCron,
+		)
+		if err != nil {
+			return fmt.Errorf("unable to update profile %s: %w", profileID, err)
+		}
+
+		fmt.Printf("Profile %s successfully updated\n", profileID)
+
+		return nil
+	},
+}
+
+var profileUpdateSchema = &cobra.Command{
+	Use:     "update-schema profile-id",
+	Aliases: []string{"update-schema"},
+	Short:   "Update a profile schema",
+	Long:    `Update a profile schema by profileID.`,
+	Example: `escape-cli profiles update-schema 00000000-0000-0000-0000-000000000001 --schema "https://example.com/schema.json"`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		profileID := args[0]
+		err := escape.UpdateProfileSchema(cmd.Context(), profileID, &newProfileSchemaURL)
+		if err != nil {
+			return fmt.Errorf("unable to update profile schema %s: %w", profileID, err)
+		}
+
+		fmt.Printf("Profile %s successfully updated\n", profileID)
+
+		return nil
+	},
+}
+
+var profileUpdateConfiguration = &cobra.Command{
+	Use:     "update-config profile-id",
+	Aliases: []string{"update-configuration"},
+	Short:   "update a profile configuration",
+	Long:    `update a profile configuration by profileID.`,
+	Example: `escape-cli profiles update-config 00000000-0000-0000-0000-000000000001 --config "{\"key\": \"value\"}"`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		profileID := args[0]
+		err := escape.UpdateProfileConfiguration(cmd.Context(), profileID, &newProfileConfiguration)
+		if err != nil {
+			return fmt.Errorf("unable to update profile configuration %s: %w", profileID, err)
+		}
+
+		fmt.Printf("Profile %s successfully updated\n", profileID)
+
+		return nil
+	},
+}
+
 func init() {
 	profilesCmd.AddCommand(
 		profilesListCmd,
 		profileGetCmd,
+		profileDeleteCmd,
+		profileUpdateCmd,
+		profileUpdateSchema,
+		profileUpdateConfiguration,
 	)
+	profileUpdateCmd.Flags().StringVarP(&newProfileName, "name", "n", "", "name of the profile")
+	profileUpdateCmd.Flags().StringVarP(&newProfileCron, "cron", "c", "", "cron of the profile")
+	profileUpdateSchema.Flags().StringVarP(&newProfileSchemaURL, "schema", "s", "", "schema of the profile")
+	profileUpdateConfiguration.Flags().StringToStringVarP(&newProfileConfiguration, "configuration", "c", map[string]string{}, "configuration of the profile")
 	rootCmd.AddCommand(profilesCmd)
 }
