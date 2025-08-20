a55431fc109b98fe7bd0cb4c87bf5eb5fecb820f
Kaan Doyurur <kaan@escape.tech>
2025-08-20 14:08:22 +0200

    --wip-- [skip ci]

--- pkg/cli/cmd/events.go
+++ pkg/cli/cmd/events.go
@@ -27,13 +27,13 @@ var eventsCmd = &cobra.Command{
 var eventsListCmd = &cobra.Command{
 	Use:     "list",
 	Aliases: []string{"ls"},
-	Short:   "List issues",
-	Long: `List issues.
+	Short:   "List events",
+	Long: `List events.
 
 Example output:
 ID                                      LEVEL    TITLE                      STAGE            CREATED AT
 00000000-0000-0000-0000-000000000001    INFO     Scan started              	EXECUTION        2025-08-12T14:04:58.117Z`,
-	Example: `escape-cli issues list`,
+	Example: `escape-cli events list`,
 	RunE: func(cmd *cobra.Command, _ []string) error {
 		events, next, err := escape.ListEvents(cmd.Context(), "", eventLevels)
 		if err != nil {
@@ -92,44 +92,12 @@ ID                                      LEVEL    TITLE                          
 	},
 }
 
-// Todo(@ValentinR01): Rework fields displayed
-var eventGetAttachmentsCmd = &cobra.Command{
-	Use:     "attachments",
-	Aliases: []string{"a"},
-	Short:   "Get attachments of an event",
-	Long: `Get attachments of an event.
-
-Example output:
-ID                                      TYPE         NAME                                                           CREATED AT              HAS CI    CRON
-00000000-0000-0000-0000-000000000001    REST         Example-Application-1                                          2025-02-21T11:15:07Z    false     0 11 * * 5`,
-	Example: `escape-cli events attachments event-id`,
-	Args:    cobra.ExactArgs(1),
-	RunE: func(cmd *cobra.Command, args []string) error {
-		eventID := args[0]
-		attachments, err := escape.GetEventAttachments(cmd.Context(), eventID)
-		if err != nil {
-			return fmt.Errorf("unable to get attachments: %w", err)
-		}
-
-		result := []string{"ID\tCREATED AT\tEXCHANGE\tTARGET\tSCREENSHOT\tSNIPPET"}
-		for _, attachment := range attachments {
-			result = append(result, fmt.Sprintf("%s\t%s\t%s\t%s\t%s\t%s", attachment.GetId(), attachment.GetCreatedAt(), attachment.GetExchange().Curl, attachment.GetTarget().AdditionalProperties, attachment.GetScreenshot(), attachment.GetSnippet()))
-		}
-
-		out.Table(result, func() []string {
-			return result
-		})
-
-		return nil
-	},
-}
 
 func init() {
 	eventsCmd.AddCommand(eventsListCmd)
 	eventsListCmd.Flags().StringSliceVarP(&eventLevels, "levels", "l", eventLevels, fmt.Sprintf("levels of events: %v", v3.AllowedENUMPROPERTIESDATAITEMSPROPERTIESLEVELEnumValues))
 
 	eventsCmd.AddCommand(eventGetCmd)
-	eventsCmd.AddCommand(eventGetAttachmentsCmd)
 
 	rootCmd.AddCommand(eventsCmd)
 }
