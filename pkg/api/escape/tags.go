0080fd322b2d77b0c5dbac307a496955b46c389e
Kaan Doyurur <kaan@escape.tech>
2025-08-21 19:06:05 +0200

    feat: tags
    Signed-off-by: Kaan Doyurur <kaan@escape.tech>

---
+++ pkg/api/escape/tags.go
@@ -0,0 +1,39 @@
+package escape
+
+import (
+	"context"
+	"fmt"
+
+	v3 "github.com/Escape-Technologies/cli/pkg/api/v3"
+)
+
+// ListTags lists all tags
+func ListTags(ctx context.Context) ([]v3.TagDetail, error) {
+	client, err := newAPIV3Client()
+	if err != nil {
+		return nil, fmt.Errorf("unable to init client: %w", err)
+	}
+	req := client.TagsAPI.ListTags(ctx)
+	data, _, err := req.Execute()
+	if err != nil {
+		return nil, fmt.Errorf("api error: %w", err)
+	}
+	return data, nil
+}
+
+// CreateTag creates a tag
+func CreateTag(ctx context.Context, name string, color string) (*v3.TagDetail, error) {
+	client, err := newAPIV3Client()
+	if err != nil {
+		return nil, fmt.Errorf("unable to init client: %w", err)
+	}
+	req := client.TagsAPI.CreateTag(ctx)
+	data, _, err := req.CreateTagRequest(v3.CreateTagRequest{
+		Name: name,
+		Color: color,
+	}).Execute()
+	if err != nil {
+		return nil, fmt.Errorf("api error: %w", err)
+	}
+	return data, nil
+}
