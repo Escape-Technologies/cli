31455e7e58346468ba7ad3fdea720abd0c83cd98
Kaan Doyurur <kaan@escape.tech>
2025-09-24 18:08:42 +0200

    fix: rename
    Signed-off-by: Kaan Doyurur <kaan@escape.tech>

--- pkg/env/color.go
+++ pkg/env/color.go
@@ -6,10 +6,7 @@ import (
 
 // GetColorPreference returns the color preference
 // Default to true if not set
-func GetColorPreference() (bool, error) {
-	noColor := os.Getenv("ESCAPE_NO_COLOR")
-	if noColor == "true" {
-		return true, nil
-	}
-	return false, nil
+func GetColorPreference() (bool) {
+	isColorDisabled := os.Getenv("ESCAPE_COLOR_DISABLED")
+	return isColorDisabled == "true"
 }
