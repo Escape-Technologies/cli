3255dbb942ecb1c49098aa233cb856c5a807385d
QuentinN42 <quentin@lieumont.fr>
2026-06-09 19:31:40 +0200

    fix: fmt
    Signed-off-by: QuentinN42 <quentin@lieumont.fr>

--- pkg/locations/dns/serve.go
+++ pkg/locations/dns/serve.go
@@ -1,6 +1,10 @@
 package dns
 
-import "github.com/miekg/dns"
+import (
+	"fmt"
+
+	"github.com/miekg/dns"
+)
 
 // serve runs a TCP DNS server bound to addr until it stops. It blocks and
 // returns the error that caused ListenAndServe to exit.
@@ -10,5 +14,9 @@ func serve(addr string, handler dns.Handler) error {
 		Net:     "tcp",
 		Handler: handler,
 	}
-	return server.ListenAndServe()
+	err := server.ListenAndServe()
+	if err != nil {
+		return fmt.Errorf("failed to listen and serve DNS server: %w", err)
+	}
+	return nil
 }
